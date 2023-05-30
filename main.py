01a94bb10abe633fd573c75f7000cf4987178783
IvanowAndrew22md17 <125370213+IvanowAndrew22md17@users.noreply.github.com>
2023-05-30 06:58:43 +0300

    add

--- main.py
+++ main.py
@@ -5,70 +5,70 @@ x = int(input("введите номер задания:"))
 #12.1
 def f1():
     class Restaurant:
-        def __init__(self, name, cuisine_type):
+        def __init__(self, name, k_type):
             self.name = name
-            self.cuisine_type = cuisine_type
+            self.k_type = k_type
         def describe_restaurant(self):
-            print(f"{self.name} is a {self.cuisine_type} restaurant.")
+            print(f"Название ресторана:{self.name} тип кухни:{self.k_type}")
     class IceCreamStand(Restaurant):
-        def __init__(self, name, cuisine_type, flavors):
-            super().__init__(name, cuisine_type)
+        def __init__(self, name, k_type, flavors):
+            super().__init__(name, k_type)
             self.flavors = flavors
-        def display_flavors(self):
-            print("У нас есть следующие вкусы мороженого:")
+        def sh_flavors(self):
+            print("У нас есть такие вкусы мороженого:")
             for flavor in self.flavors:
                 print(flavor)
-    my_ice_cream_stand = IceCreamStand("Scoops", "Ice Cream Cafe", ["ванильный", "шоколадный", "клубничный"])
-    my_ice_cream_stand.describe_restaurant()
-    my_ice_cream_stand.display_flavors()
+    new_ice_cream_stand = IceCreamStand("Baskin-Robbins", "Ice Cream", ["ванильный", "шоколадный", "фисташковое"])
+    new_ice_cream_stand.describe_restaurant()
+    new_ice_cream_stand.sh_flavors()
 if x == 1:
     f1()
 
 # 12.2
 def f2():
     class IceCreamStand:
-        def __init__(self, name, flavors, location, working_hours, type_flavors):
+        def __init__(self, name, flavors, location, w_hours, type):
             self.name = name
             self.flavors = flavors
             self.location = location
-            self.working_hours = working_hours
-            self.type_flavors = type_flavors
+            self.w_hours = w_hours
+            self.type = type
         def show_flavors(self):
-            print("Список доступных сортов мороженого:")
+            print("Список вкусов мороженого:")
             for flavor in self.flavors:
                 print("- " + flavor)
         def add_flavor(self, flavor):
             self.flavors.append(flavor)
-            print(f'Сорт {flavor} добавлен в список')
+            print(f'Вкус {flavor} добавлен в список')
         def remove_flavor(self, flavor):
             if flavor in self.flavors:
                 self.flavors.remove(flavor)
-                print(f'Сорт {flavor} удален из списка')
+                print(f'Вкус {flavor} удален из списка')
             else:
-                print(f'Сорт {flavor} не найден в списке')
+                print(f'Вкус {flavor} не найден в списке')
         def check_flavor(self, flavor):
             if flavor in self.flavors:
-                print(f'Сорт {flavor} есть в списке')
+                print(f'Вкус {flavor} есть в списке')
             else:
-                print(f'Сорта {flavor} нет в списке')
+                print(f'Вкус {flavor} нет в списке')
         def describe_stand(self):
             print(f"Стенд мороженного {self.name}")
-            print(f"Место: {self.location}")
-            print(f"Время работы: {self.working_hours}")
-        def show_Typesflavors(self):
+            print(f"Адрес: {self.location}")
+            print(f"Часы работы: {self.w_hours}")
+        def show_Types(self):
             print("Список доступных типов мороженого:")
-            for types in self.type_flavors:
+            for types in self.type:
                 print("- " + types)
-    my_stand = IceCreamStand("Кафе-мороженное", ['ванильное', 'шоколадное', 'клубничное'], 'Ударников проспект','10.00 - 17.00', ['эскимо', 'стакан'])
-    my_stand.describe_stand()
-    my_stand.show_flavors()
-    my_stand.show_Typesflavors()
-    my_stand.check_flavor("клубничное")
-    my_stand.check_flavor("ванильное")
-    my_stand.add_flavor("фисташковое")
-    my_stand.check_flavor("шоколадное")
-    my_stand.remove_flavor("клубничное")
-    my_stand.check_flavor("ванильное")
+    new_stand = IceCreamStand("Carvel", ['ванильное', 'шоколадное', 'фисташковое'], 'Благодатная улица 30','12.00 - 23:00', ['на палочке', 'стакан', 'рожок'])
+    new_stand.describe_stand()
+    new_stand.show_flavors()
+    new_stand.show_Types()
+    new_stand.add_flavor("клубничное")
+    new_stand.check_flavor("фисташковое")
+    new_stand.check_flavor("клубничное")
+    new_stand.remove_flavor("фисташковое")
+    new_stand.check_flavor("фисташковое")
+    new_stand.check_flavor("шоколадное")
 if x==2:
     f2() 
 
@@ -78,17 +78,18 @@ def f3():
         def __init__(self, name, flavors):
             self.name = name
             self.flavors = flavors
-        def get_menu(self):
-            menu = "Добро пожаловать в наше кафе-мороженое " + self.name + "\n"
-            menu += "У нас есть следующие сорты мороженого:\n"
+        def g_menu(self):
+            menu = "Добро пожаловать в " + self.name + "\n"
+            menu += "У нас есть такие вкусы мороженого:\n"
             for flavor in self.flavors:
                 menu += "- " + flavor + "\n"
             return menu
-    my_stand = IceCreamStand("Мир морожки", ["ванильный", "шоколадный", "клубничный"])
+    my_stand = IceCreamStand("Kona ice", ['ванильное', 'шоколадное', 'фисташковое'])
     root = tk.Tk()
-    root.title("Мир сладкого")
-    root.geometry('400x250')
-    menu_label = tk.Label(root, text=my_stand.get_menu())
+    root['bg'] = 'blue3'
+    root.title("IceCreamStand")
+    root.geometry('300x250')
+    menu_label = tk.Label(root, text=my_stand.g_menu(), bg='pink')
     menu_label.pack()
     root.mainloop()
 if x==3:
